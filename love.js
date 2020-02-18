c9d8ffd16ef0ccb4d454cb561e126437d6ce91ad
Rohit Jakhar <snjdrohit940@gmail.com>
2020-02-18 19:50:56 +0530

    add external javascript and work almost done but we need to refresh everytime

---
+++ love.js
@@ -0,0 +1,22 @@
+var one = document.getElementById("first").value;
+var two = document.getElementById("second").value;
+var first = 0;
+var second = 0;
+
+
+for(var i=0; i<one.length; i++){
+    first = first+one.charCodeAt(i);
+}
+
+for(var i=0; i<two.length; i++){
+    second = second + two.charCodeAt(i);
+}
+
+result = (first+second)%100;
+if(result > 100){
+    result = 100;
+}
+function calcu(){
+document.getElementById('cal').value = result;
+}
+console.log(result);
